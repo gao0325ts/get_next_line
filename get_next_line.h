ba41993cb933de9fb4cac46316c8db6f13830b98
stakada <stakada@student.42tokyo.jp>
2024-05-16 18:58:25 +0900

    init

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/16 18:50:50 by stakada           #+#    #+#             */
+/*   Updated: 2024/05/16 18:55:15 by stakada          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+char *get_next_line(int fd);
+
+#endif
