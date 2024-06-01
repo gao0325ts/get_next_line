2e7687df4f1787f2403c50f10799e3c1989f45c0
stakada <stakada@student.42tokyo.jp>
2024-06-01 16:03:40 +0900

    fix strlen

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: stakada <stakada@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 18:53:15 by stakada           #+#    #+#             */
-/*   Updated: 2024/06/01 15:37:41 by stakada          ###   ########.fr       */
+/*   Updated: 2024/06/01 16:02:40 by stakada          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 size_t	ft_strlen_gnl(char *s)
 {
-	ssize_t	len;
+	size_t	len;
 
 	len = 0;
 	while (s && s[len])
